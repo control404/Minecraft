f1e5347e1bf948051af4bb1cc29507c54f35d2a4
control404 <kil13022005@gmail.com>
2026-05-02 01:17:41 +0300

    Add files via upload

--- api/go.js
+++ api/go.js
@@ -1,12 +1,13 @@
 export default function handler(req, res) {
-    const botToken = '8606085102:AAGYTrjx6BjMYh_GXtw7o1vRz4zSiieHMCA';
-    const chatId = '-1003769181399';
-    
-    const text = encodeURIComponent("🚀 Клик по ссылке в био! \n🔗 Сайт: craftdudl.site");
+  const botToken = '8606085102:AAGYTrjx6BjMYh_GXtw7o1vRz4zSiiehMCA';
+  const chatId = '-1003769181399';
+  const text = encodeURIComponent("🚀 Клик по ссылке в био craftdudl.site!");
 
-    fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${text}`)
-        .catch(err => console.error("Ошибка ТГ"));
+  // Отправляем уведомление (не ждем ответа, чтобы не тормозить юзера)
+  fetch(`https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&text=${text}`)
+    .catch(err => console.error("Ошибка ТГ"));
 
-    res.setHeader('Location', 'https://craftdudl.site');
-    res.status(302).end();
+  // Редирект на твой основной сайт (index.html)
+  res.setHeader('Location', 'https://craftdudl.site');
+  res.status(302).end();
 }
